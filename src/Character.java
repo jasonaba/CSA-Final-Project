0ba1c4ae42fcc5570d24d63e7c64f51459263afa
jason <jason@localhost>
2026-05-02 21:29:14 -0400

    Initial Commit: Character class skeleton (#4)

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,18 @@
 
 public class Character {
-
+	//Variables
+	private int x, y, xVel, yVel;
+	private String color;
+	static boolean twoPlayer;
+	
+	//Constructors
+	public Character(int x, int y, int xVel, int yVel, String color, boolean twoPlayer) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.xVel = xVel;
+		this.yVel = yVel;
+		this.color = color;
+	}
+	
 }
