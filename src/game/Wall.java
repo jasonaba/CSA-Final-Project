277ba594cd2df57e861df72f2c9157a38578ff44
jasonaba <abankwahjason@gmail.com>
2026-05-19 07:48:24 -0400

    Add files via upload

---
+++ src/game/Wall.java
@@ -0,0 +1,44 @@
+package game;
+
+public class Wall implements Tile{
+	private int x, y, width, height;	
+	
+	public Wall(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return y;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return height;
+	}
+
+	@Override
+	public boolean isColliding(Character c) {
+		// TODO Auto-generated method stub
+		return if(c.getX() + .getWidth()==);
+	}
+
+}
